fbd06b102bb48e5218d1bcf0477f856799764cc6
datta <dattu99rockstar@gmail.com>
2026-01-05 21:02:23 +0530

    Add translation and favicon files for the application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild, } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-
+import { RouterOutlet } from '@angular/router'
+import { ViewChild } from '@angular/core';;
+import translations  from '../assets/translation.json'
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,FormsModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'NaraLaw';
+  api_key='AIzaSyCAZm_CP8DgGeVkEb6Ct5Hy_E0IvhEZrSI';
+   ts=translations;
+  @ViewChild('booking') booking!:ElementRef;
+  title = translations.heading;
+  client_num=''
+  client_casetype=''
+  client_name=''
+  title_1=translations.title
+  pargrah=this.ts.paragraph
+  book_your_slot=this.ts.book_your_slot
+  lawyer_name=this.ts.Lawyer_name
+ client_details:any=[]
+  Book_a_Consultation=this.ts['Book a Consultation']
+  case_types = [
+  'CIVIL',
+  'CRIMINAL',
+  'FAMILY',
+  'PROPERTY',
+  'BUSINESS',
+  'LABOUR',
+  'INTELLECTUAL PROPERTY',
+  'TAX'
+];
+
+  submit(){
+    //const sectionEl = this.booking.nativeElement;
+    this.booking.nativeElement.scrollIntoView({
+      behavior: 'smooth',
+      block: 'center'
+    });
+
+    alert('worked')
+  }
+  client_Details(){
+    this.client_details=[this.client_name,this.client_num,this.client_casetype]
+    console.log(this.client_details)
+    console.log(this.client_name)
+    console.log(this.client_num)
+    console.log(this.client_casetype)
+  }
 }
