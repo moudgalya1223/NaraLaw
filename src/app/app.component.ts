84511f2d7475c9e4465aa308437c7d5fd170ee8d
moudgalya1223 <soulrepresentors@gmail.com>
2026-01-09 11:20:42 +0530

    fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -104,15 +104,12 @@ upipayment(){
 
   const note = `Online Consultation - ${this.selectedPlan.duration}`;
 
-  const upiUrl = `upi://pay?pa=${upiId}` +
-  `&pn=${encodeURIComponent(payeeName)}` +
-  `&tr=${transactionRef}` +        // optional but recommended
-  `&tn=${encodeURIComponent(note)}` +
-  `&am=${Number(amount).toFixed(2)}` +
-  `&cu=INR` +
-  `&url=${encodeURIComponent(callbackUrl || '')}`;  // optional
-;
-
+  const upiUrl =
+    `upi://pay?pa=${upiId}` +
+    `&pn=${encodeURIComponent(payeeName)}` +
+    `&am=${amount}` +
+    `&cu=INR` +
+    `&tn=${encodeURIComponent(note)}`;
   window.location.href = upiUrl;
 }
 
